0b85ec289ecb62e8fe11276a48c8fdc20a54d6bd
kelseymara <77584252+kelseymara@users.noreply.github.com>
2022-09-20 23:34:01 -0400

    Create BFS.cpp

---
+++ trees/traversalTechniques/BFS.cpp
@@ -0,0 +1,13 @@
+// from https://www.youtube.com/watch?v=VieXU-OUtxU
+
+/*
+algorithm:
+push root into queue
+while queue is not empty:
+  pick root 
+  pop the root (and print it) 
+  push its neighbors
+  /*
+          
+  
+  
